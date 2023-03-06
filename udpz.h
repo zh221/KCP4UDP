f362ff37afc06e48a25c210b67d3e2f09cabbd1a
zh221 <2690680313@qq.com>
2023-03-06 22:51:39 +0800

    [Add]: add new method for trans.

--- udpz.h
+++ udpz.h
@@ -13,6 +13,21 @@
 #include "netinet/in.h"
 #include "arpa/inet.h"
 
+#define MSG_DAT_PIECE_LEN 6388
+#define MSG_TIMEOUT 30
+
+struct MsgPiece
+{
+    unsigned short conv;
+    unsigned short totalSize;
+    unsigned short totalPiece;
+    unsigned short currPiece;
+    unsigned int timeSlab;
+    data[MSG_DAT_PIECE_LEN];
+}
+
+const unsigned int MSG_SUB_PKG_LEN = sizeof(MsgPiece);
+
 class UdpZ
 {
 public:
@@ -75,6 +90,93 @@ public:
         }
         return 0;
         
+    }
+private:
+    int DetachSend(const char *msgData, int msgLen)
+    {
+        int totalSize = msgLen;
+        int totalPiece = msgLen / MSG_DAT_PIECE_LEN;
+        if (msgLen % MSG_DAT_PIECE_LEN) {
+            totalPiece += 1;
+        }
+        int ret = -1;
+        char buf[MSG_SUB_PKG_LEN];
+        MsgPiece msgPiece;
+        for (int i = 0; i < totalPiece - 1; i++)
+        {
+            memcpy(&msgPiece.data, msgData + i * MSG_DAT_PIECE_LEN, MSG_DAT_PIECE_LEN);
+            msgPiece.conv = 0x01;
+            msgPiece.totalSize = msgLen;
+            msgPiece.totalPiece = totalPiece;
+            msgPiece.currPiece = i;
+            msgPiece.timeSlab = TimeSlab();
+            bzero(buf, MSG_SUB_PKG_LEN);
+            memcpy(buf, &msgPiece, MSG_SUB_PKG_LEN);
+            ret = sendto(this->_sockfd, buf, MSG_SUB_PKG_LEN, (const sockaddr*)&this->_remoteAddr,
+                         sizeof(this->_remoteAddr));
+            if (ret < 0) {
+                return -1;
+            }
+        }
+        int len = msgLen - MSG_DAT_PIECE_LEN * (totalPiece - 1);
+        memcpy(&msgPiece.data, msgData + (totalPiece - 1) * MSG_DAT_PIECE_LEN, len);
+        msgPiece.conv = 0x01;
+        msgPiece.totalSize = msgLen;
+        msgPiece.totalPiece = totalPiece;
+        msgPiece.currPiece = totalPiece - 1;
+        msgPiece.timeSlab = TimeSlab();
+        bzero(buf, MSG_SUB_PKG_LEN);
+        memcpy(buf, &msgPiece, MSG_SUB_PKG_LEN);
+        ret = sendto(this->_sockfd, buf, MSG_SUB_PKG_LEN, (const sockaddr*)&this->_remoteAddr,
+                     sizeof(this->_remoteAddr));
+        if (ret < 0) {
+            return -1;
+        }
+        return msgLen;
+    }
+    int AssembleRecv(char *msgData, int &msgLen)
+    {
+        int ret = -1;
+        socklen_t remoteAddrLen = sizeof(this->_remoteAddr);
+        char buf[MSG_SUB_PKG_LEN] = {0};
+        while (ret < 0) {
+            ret = recvfrom(this->_sockfd, buf, sizeof(buf), (sockaddr *)&this->_remoteAddr, 0, 
+                           (struct sockaddr*)&this->_remoteAddr, &remoteAddrLen);
+        }
+        MsgPiece *msgPiece = static_cast<MsgPiece *>(buf);
+        unsigned short conv = msgPiece->conv
+        std::vector<MsgPiece> msgPieceList(msgPiece->totalPiece);
+        msgPieceList[msgPiece->currPiece] = *msgPiece;
+        unsigned int lastTimeSlab = msgPiece->timeSlab;
+        unsigned int newTimeSlab = msgPiece->timeSlab;
+        int counter = 1;
+        while (counter < msgPieceList.size()) {
+            ret = -1;
+            bzero(buf, MSG_SUB_PKG_LEN);
+            ret = recvfrom(this->_sockfd, buf, sizeof(buf), (sockaddr *)&this->_remoteAddr, 0, 
+                           (struct sockaddr*)&this->_remoteAddr, &remoteAddrLen);
+            if (ret > 0) {
+                msgPiece = static_cast<MsgPiece *>(buf);
+                if (msgPiece->conv == conv)
+                {
+                    msgPieceList[msgPiece->currPiece] = *msgPiece;
+                    counter++;
+                    lastTimeSlab = msgPiece->timeSlab;
+                }
+            }
+            newTimeSlab = TimeSlab();
+            if (lastTimeSlab - newTimeSlab > MSG_TIMEOUT) {
+                return -1;
+            }
+        }
+
+        return msgLen;
+    }
+private:
+    unsigned int TimeSlab()
+    {
+        // ...
+        return 0;
     }
 private:
     int _sockfd = -1;
